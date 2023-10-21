3baff6ae7067cfb2ad452ea4772634602d6ff92c
Earl Jan Paquibut <earlpaquibut@gmail.com>
2023-10-21 20:56:14 +0800

    Sidebar and Dashboard

---
+++ ASI.Basecode.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using ASI.Basecode.Data.Interfaces;
+using ASI.Basecode.Services.Interfaces;
+using ASI.Basecode.WebApp.Mvc;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System.Linq;
+
+namespace ASI.Basecode.WebApp.Controllers
+{
+    
+    /// <summary>
+    /// Home Controller
+    /// </summary>
+    public class HomeController : ControllerBase<HomeController>
+    {
+        private readonly IUserService _userService;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="httpContextAccessor"></param>
+        /// <param name="loggerFactory"></param>
+        /// <param name="configuration"></param>
+        /// <param name="localizer"></param>
+        /// <param name="mapper"></param>
+        public HomeController(IHttpContextAccessor httpContextAccessor,
+                              ILoggerFactory loggerFactory,
+                              IConfiguration configuration,
+                              IUserService userService,
+                              IMapper mapper = null) : base(httpContextAccessor, loggerFactory, configuration, mapper)
+        {
+            _userService = userService;
+        }
+
+        /// <summary>
+        /// Returns Home View.
+        /// </summary>
+        /// <returns> Home View </returns>
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Test2()
+        {
+            return View();
+        }
+    }
+}
