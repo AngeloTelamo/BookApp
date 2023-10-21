d5f2055ffc67e2a8b8950b8937e62b54834703cb
TecsAke <aketecson@gmail.com>
2023-10-21 20:40:41 +0800

    login_registration_forgotpass

---
+++ ASI.Basecode.Services/Interfaces/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using ASI.Basecode.Data.Models;
+using ASI.Basecode.Services.ServiceModels;
+using System.Collections.Generic;
+using static ASI.Basecode.Resources.Constants.Enums;
+
+namespace ASI.Basecode.Services.Interfaces
+{
+    public interface IUserService
+    {
+        LoginResult AuthenticateUser(string userid, string password, ref User user);
+        UserEditViewModel GetUser(string userId);
+        UserListViewModel GetUserList(UserListViewModel model);
+        void AddUser(UserViewModel model);
+        void UpdateUser(UserEditViewModel model);
+        void DeleteUser(string userId);
+    }
+}
