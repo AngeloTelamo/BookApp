73a5edd631658e284fd261f0f343882f3b98950d
Earl Jan Paquibut <earlpaquibut@gmail.com>
2023-11-17 21:21:03 +0800

    Admin-Dashboard UI with model

--- ASI.Basecode.WebApp/Controllers/HomeController.cs
+++ ASI.Basecode.WebApp/Controllers/HomeController.cs
@@ -1,12 +1,21 @@
 ﻿using ASI.Basecode.Data.Interfaces;
 using ASI.Basecode.Services.Interfaces;
+using ASI.Basecode.Services.ServiceModels;
+using ASI.Basecode.Services.Services;
 using ASI.Basecode.WebApp.Mvc;
 using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Hosting;
+using System;
+using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
+using ASI.Basecode.Data.Models;
+using Microsoft.CodeAnalysis.CSharp;
 
 namespace ASI.Basecode.WebApp.Controllers
 {   
@@ -14,6 +23,7 @@ namespace ASI.Basecode.WebApp.Controllers
     public class HomeController : ControllerBase<HomeController>
     {
         private readonly IUserService _userService;
+        private readonly IBookMasterService _bookMasterService;
         //private readonly IBookService _bookService;
 
         /// <summary>
@@ -23,13 +33,16 @@ namespace ASI.Basecode.WebApp.Controllers
         /// <param name="loggerFactory"></param>
         /// <param name="configuration"></param>
         /// <param name="localizer"></param>
+        /// <param name="bookMasterService"></param>
         /// <param name="mapper"></param>
         public HomeController(IHttpContextAccessor httpContextAccessor,
                               ILoggerFactory loggerFactory,
                               IConfiguration configuration,
                               IUserService userService,
+                              IBookMasterService bookMasterService,
                               IMapper mapper = null) : base(httpContextAccessor, loggerFactory, configuration, mapper)
         {
+            _bookMasterService = bookMasterService;
             _userService = userService;
         }
 
@@ -39,7 +52,8 @@ namespace ASI.Basecode.WebApp.Controllers
         /// <returns> Home View </returns>
         public IActionResult Dashboard()
         {
-            return View();
+            var dataList = _bookMasterService.GetBookList(null); // naa sa BookmasterService ang logic sa list 
+            return View("Dashboard", dataList);
         }
     }
 }
